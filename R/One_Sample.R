7749ee55abf8a8e5a26610f8fa7477f70a819752
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-02-12 16:50:49 +1030

    Adding tests for RSS package

--- R/One_Sample.R
+++ R/One_Sample.R
@@ -1,12 +1,12 @@
 #' Title
 #'
-#' @param data The data for `JPS` or `RSS` sampling
+#' @param data A data frame of `JPS` or `RSS` rankings
 #' @param set_size The set size of the ranks.
-#' @param method Takes values `JPS` (the default) for Judgment Post Stratification, or `RSS` for Ranked Set Sample.
+#' @param method Takes values `JPS` (the default) for Judgment Post Stratification, or `RSS` for Ranked Set Sample. Partial matching is performed, so `J` or `R` could be used.
 #' @param confidence The confidence level to use.
 #' @param replace Logical (default `TRUE`). Sample with replacement?
 #' @param model If model is 0, it's design based inference, if model = 1, it is model based inference using super population model
-#' @param pop_size The population size. Must be provided if sampling without replacement, or if `model` is set to 'superpopulation'.
+#' @param pop_size The population size. Must be provided if sampling without replacement, or if `model` is set to 'super-population'.
 #'
 #' @return A `data.frame` with the rankings
 #' @export
@@ -27,9 +27,12 @@ OneSample <- function(data, set_size, method = c("JPS", "RSS"), confidence = 0.9
     # If model is 0, it's design based inference, if model = 1, it is model based inference using super population model
     # pop_size: nrow(data)*set_size <= pop_size, > 0, only relevant if replace = FALSE
 
+    if(set_size < 1 | is.na(set_size) | is.null(set_size) | !is.numeric(set_size)) {
+        stop("set_size must be a positive numeric value")
+    }
 
     if(!isTRUE(replace) & !isFALSE(replace)) {
-        stop("replace must take TRUE or FALSE")
+        stop("replace must be TRUE or FALSE")
     }
 
     method <- match.arg(toupper(method), c("JPS", "RSS"))
@@ -39,10 +42,10 @@ OneSample <- function(data, set_size, method = c("JPS", "RSS"), confidence = 0.9
     ## Check if the sample is Judgment-post stratified sample (JPS)
     if (method == "JPS") {
         if (!replace & missing(pop_size)) {
-            stop("The population size pop_size must be provided when sampling without replacement.")
+            stop("The population size pop_size must be provided when sampling without replacement")
         }
         if (model == 1 & missing(pop_size)) {
-            stop("The population size pop_size must be provided for superpopulation model")
+            stop("The population size pop_size must be provided for super-population model")
         }
 
         results <- JPSEF(data, set_size, replace, model, pop_size, alpha)
@@ -61,13 +64,13 @@ OneSample <- function(data, set_size, method = c("JPS", "RSS"), confidence = 0.9
             stop("The population size pop_size must be provided when sampling without replacement")
         }
         if (model == 1 & missing(pop_size)) {
-            stop("The population size pop_size must be provided for superpopulation model")
+            stop("The population size pop_size must be provided for super-population model")
         }
         RV <- data[, 2]
         GSV <- aggregate(RV, list(RV), length)$x
 
         if (length(GSV) != set_size | min(GSV) <= 1) {
-            stop("In Ranked set sampling design, first ranking method should not have less than two observations in any judgment ranking group")
+            stop("In ranked set sample, first ranking method should have at least two observations in any judgment ranking group")
         }
 
         results <- RSSEF(data, set_size, replace, model, pop_size, alpha)
--- tests/testthat/test-One_Sample.R
+++ tests/testthat/test-One_Sample.R
@@ -1,9 +1,45 @@
-test_that("onesample works", {
+test_that("onesample works with JPS", {
     skip_if(getRversion() < 3.4)
-    load("../onesample.Rdata")
-    output <- OneSample(Data,Setsize,Method,0.95, Replace,Model,N)
-  expect_equal(output$Estimate, c(9.978, 9.947, 9.867, 10.014, 9.584, 9.947))
+    load("../jps_data.Rdata")
+    expect_identical(OneSample(Data, 3, "JPS", 0.95, FALSE, 0, 600), saved_jps_output)
 })
 
+test_that("onesample works with RSS", {
+    skip_if(getRversion() < 3.4)
+    load("../rss_data.Rdata")
+    expect_identical(OneSample(Data, 3, "RSS", 0.95, FALSE, 0, 600), saved_rss_output)
+})
 
-# Test data, set_size, method = c("JPS", "RSS"), conf = 0.95, replace = TRUE, model = 0, N = NULL
+test_that("set_size is positive", {
+    expect_error(OneSample(emergence_ranks, -4, "JPS", replace = T, pop_size = 2640),
+                 "set_size must be a positive numeric value")
+    expect_error(OneSample(emergence_ranks, "4", "JPS", replace = T, pop_size = 2640),
+                 "set_size must be a positive numeric value")
+    expect_error(OneSample(emergence_ranks, NA, "JPS", replace = T, pop_size = 2640),
+                 "set_size must be a positive numeric value")
+})
+
+test_that("replace is TRUE or FALSE", {
+    expect_error(OneSample(emergence_ranks, 4, "JPS", replace = NA, pop_size = 2640),
+                 "replace must be TRUE or FALSE")
+    expect_error(OneSample(emergence_ranks, 4, "JPS", replace = "NA", pop_size = 2640),
+                 "replace must be TRUE or FALSE")
+    expect_error(OneSample(emergence_ranks, 4, "JPS", replace = 0, pop_size = 2640),
+                 "replace must be TRUE or FALSE")
+})
+
+test_that("pop_size is supplied when needed", {
+    expect_error(OneSample(emergence_ranks, 4, "JPS", replace = F, 0),
+                 "The population size pop_size must be provided when sampling without replacement")
+    expect_error(OneSample(emergence_ranks, 4, "JPS", replace = T, model = 1),
+                 "The population size pop_size must be provided for super-population model")
+})
+
+test_that("method is JPS or RSS", {
+    expect_error(OneSample(emergence_ranks, 4, method = "ABC", replace = T),
+                 "'arg' should be one of \"JPS\", \"RSS\"")
+    expect_error(OneSample(emergence_ranks, 4, method = 1, replace = T),
+                 "'arg' should be one of \"JPS\", \"RSS\"")
+})
+# Test data, conf = 0.95, model = 0, N = NULL
+
